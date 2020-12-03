e1868454ce141e221e40e4f67dddd94affab78ca
Andrey Tarantsov <andrey@tarantsov.com>
2020-12-03 15:05:05 +0300

    More patterns

--- code.go
+++ code.go
@@ -5,6 +5,7 @@ import "regexp"
 var patterns = []*regexp.Regexp{
 	regexp.MustCompile(`Код ([\d\w]+)(?:\.|\s|$)`),
 	regexp.MustCompile(`Kod: ([\d\w]+)(?:\.|\s|$)`),
+	regexp.MustCompile(`(?i)код: ([\d\w]+)(?:\.|\s|$)`),
 	regexp.MustCompile(`Код активации Apple Pay ([\d\w]+)`),
 	regexp.MustCompile(`Код подтверждения: ([\d\w]+)`),
 	regexp.MustCompile(`Код`),
